b91d7e87e02170e7a241a17adce13cc060efd521
JoseGabino12 <gabinoMorales1212@hotmail.com>
2023-05-24 17:24:51 -0600

    Status form

---
+++ packages/registro_frontend/src/views/Vehiculo/components/Info.jsx
@@ -0,0 +1,48 @@
+import { Text } from '@tremor/react'
+import { useEffect, useState } from 'react'
+
+export default function Info ({ vehiculo }) {
+  const [entrada, setEntrada] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('entrada', entrada)
+    setEntrada(localStorage.getItem('entrada'))
+  }, [entrada])
+
+  const handleEntrada = () => {
+    setEntrada(!entrada)
+  }
+  return (
+    <div className='flex flex-row gap-5'>
+      <img src={vehiculo.imagen} className='h-52 w-auto bg-cover' alt='Carro Mazda 3' />
+      <div className='flex flex-col gap-3'>
+        <Text>
+          <strong>Marca:</strong> {vehiculo.marca}
+        </Text>
+        <Text>
+          <strong>Modelo:</strong> {vehiculo.modelo}
+        </Text>
+        <Text>
+          <strong>Año:</strong> {vehiculo.anio}
+        </Text>
+        <Text>
+          <strong>Empresa:</strong> {vehiculo.empresa}
+        </Text>
+        <Text>
+          <strong>Número económico:</strong> {vehiculo.numeconomico}
+        </Text>
+      <div className='flex flex-col'>
+        {
+          entrada
+            ? (
+              <button className='bg-green-600 text-white rounded-lg p-2' onClick={handleEntrada}>Agregar salida</button>
+              )
+            : (
+              <button className='bg-green-600 text-white rounded-lg p-2' onClick={handleEntrada}>Agregar entrada</button>
+              )
+        }
+      </div>
+      </div>
+    </div>
+  )
+}
---
+++ packages/registro_frontend/src/views/form/components/Status.jsx
@@ -0,0 +1,7 @@
+export default function StatusInfo ({ status }) {
+  return (
+    <div>
+      <h1>{status}</h1>
+    </div>
+  )
+}
