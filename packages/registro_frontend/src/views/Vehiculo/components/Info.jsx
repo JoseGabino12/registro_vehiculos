ae96ed176e428706c1983e0813ed76f54151d97e
JoseGabino12 <gabinoMorales1212@hotmail.com>
2023-05-24 20:42:59 -0600

    Status in info car

--- packages/registro_frontend/src/views/Vehiculo/components/Info.jsx
+++ packages/registro_frontend/src/views/Vehiculo/components/Info.jsx
@@ -1,12 +1,23 @@
 import { Text } from '@tremor/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Info ({ vehiculo }) {
-  const [entrada, setEntrada] = useState(false)
+  const [entrada, setEntrada] = useState(null)
 
-  const handleEntrada = () => {
-    setEntrada(!entrada)
+  const handleEntrada = async () => {
+    await fetch(`http://localhost:4000/api/auto/${vehiculo.id}`, {
+      method: 'PUT'
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setEntrada(data.status)
+      })
   }
+
+  useEffect(() => {
+    setEntrada(vehiculo.status)
+  }, [vehiculo.status])
+
   return (
     <div className='flex flex-row gap-5'>
       <img src={vehiculo.imagen} className='h-52 w-auto bg-cover' alt='Carro Mazda 3' />
@@ -27,15 +38,9 @@ export default function Info ({ vehiculo }) {
           <strong>Número económico:</strong> {vehiculo.numeconomico}
         </Text>
       <div className='flex flex-col'>
-        {
-          entrada
-            ? (
-              <button className='bg-green-600 text-white rounded-lg p-2' onClick={handleEntrada}>Agregar salida</button>
-              )
-            : (
-              <button className='bg-green-600 text-white rounded-lg p-2' onClick={handleEntrada}>Agregar entrada</button>
-              )
-        }
+        <button className={entrada ? 'bg-green-600 text-white rounded-lg p-2' : 'bg-red-600 text-white rounded-lg p-2'} onClick={handleEntrada}>
+          {entrada ? 'Agregar entrada' : 'Agregar salida'}
+        </button>
       </div>
       </div>
     </div>
