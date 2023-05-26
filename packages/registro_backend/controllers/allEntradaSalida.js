6b97cf90dd633408c5cc77eb70f960eff170d124
JuanMichel16 <juan.michel16@outlook.com>
2023-05-26 03:00:23 -0600

    adding a better error handler

--- packages/registro_backend/controllers/allEntradaSalida.js
+++ packages/registro_backend/controllers/allEntradaSalida.js
@@ -2,33 +2,41 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
 
-const AllEntradasSalidas = async (req, res) => {
-    const entradas = await prisma.entrada.findMany();
-    const salidas = await prisma.salida.findMany();
+const AllEntradasSalidas = async (req, res, next) => {
 
-    const io = [{entradas: entradas}, {salidas: salidas}];
+    try {
+        const entradas = await prisma.entrada.findMany({});
+        const salidas = await prisma.salida.findMany({});
+        const io = [{entradas: entradas}, {salidas: salidas}];
 
-    return res.json(io);
+        return res.status(200).json(io);
+    } catch (error) {
+        next(error);
+    }
 
 }
 
-const allEntradasSalidasById = async (req, res) => {
-  const { id } = req.params;
-  const idNumber = Number(id);
+const allEntradasSalidasById = async (req, res, next) => {
+    const { id } = req.params;
+    const idNumber = Number(id);
 
-  const entradas = await prisma.entrada.findMany({
-      where:{
-          autoId: idNumber
-      }
-  })
-  const salidas = await prisma.salida.findMany({
-      where:{
-          autoId: idNumber
-      }
-  })
+    try {
+        const entradas = await prisma.entrada.findMany({
+            where:{
+                autoId: idNumber
+            }
+        })
+        const salidas = await prisma.salida.findMany({
+            where:{
+                autoId: idNumber
+            }
+        })
 
-  const io = [{entradas: entradas}, {salidas: salidas}];
-  res.json({io})
+        const io = [{entradas: entradas}, {salidas: salidas}];
+        res.json({io})
+    } catch (error) {
+        next(error);
+    }
 
 }
 
